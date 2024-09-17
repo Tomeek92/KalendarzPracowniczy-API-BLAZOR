cf1a4370bd3e25dc639d52ac0b486b30035d467b
Tomasz Fryckowski <tomaszfryckowski@gmail.com>
2024-09-17 13:45:31 +0200

    add Dependancy Injection (Work)

--- KalendarzPracowniczyApplication/Extensions/DependancyInjection.cs
+++ KalendarzPracowniczyApplication/Extensions/DependancyInjection.cs
@@ -7,11 +7,16 @@ using KalendarzPracowniczyApplication.CQRS.Commands.Users.Update;
 using KalendarzPracowniczyApplication.CQRS.Commands.Workers.Create;
 using KalendarzPracowniczyApplication.CQRS.Commands.Workers.Delete;
 using KalendarzPracowniczyApplication.CQRS.Commands.Workers.Update;
+using KalendarzPracowniczyApplication.CQRS.Commands.Works.Create;
+using KalendarzPracowniczyApplication.CQRS.Commands.Works.Delete;
+using KalendarzPracowniczyApplication.CQRS.Commands.Works.Update;
 using KalendarzPracowniczyApplication.CQRS.Queries.Events.GetAll;
 using KalendarzPracowniczyApplication.CQRS.Queries.Events.GetElementById;
 using KalendarzPracowniczyApplication.CQRS.Queries.Users.GetUserById;
 using KalendarzPracowniczyApplication.CQRS.Queries.Workers.GetAllWorkers;
 using KalendarzPracowniczyApplication.CQRS.Queries.Workers.GetWorkerById;
+using KalendarzPracowniczyApplication.CQRS.Queries.Works.GetUserTaskById;
+using KalendarzPracowniczyApplication.CQRS.Queries.Works.GetUserTasks;
 using KalendarzPracowniczyApplication.Mapper;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -35,7 +40,12 @@ namespace KalendarzPracowniczyApplication.Extensions
         typeof(GetAllWorkersQuery).Assembly,
         typeof(CreateWorkerCommand).Assembly,
         typeof(UpdateWorkerCommand).Assembly,
-        typeof(DeleteWorkerCommand).Assembly
+        typeof(DeleteWorkerCommand).Assembly,
+        typeof(CreateWorkCommand).Assembly,
+        typeof(DeleteWorkCommand).Assembly,
+        typeof(UpdateWorkCommand).Assembly,
+        typeof(GetUserTasksQuery).Assembly,
+        typeof(GetUserTaskByIdQuery).Assembly
            ));
 
             services.AddAutoMapper(typeof(MapperProfile));
